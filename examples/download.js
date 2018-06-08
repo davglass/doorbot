d8dd9a67f3fd6912b4d8ecaaff48f0c5cc72a68e
Wee Liat <wl@hackertrail.com>
2018-06-08 14:46:30 +0800

    last fix, added historyLimit setting

--- examples/download.js
+++ examples/download.js
@@ -14,35 +14,38 @@ const path = require('path');
 const url = require('url');
 const request = require('request');
 
+/*
+ * Configure your settings here:
+ * email, password, historyLimit
+ */
 const ring = RingAPI({
     email: 'your@email.com',
     password: '12345'
 });
+const historyLimit = 1000;
 
 const base = path.join(__dirname, 'downloads');
 
 fs.mkdir(base, () => { //ignoring if it exists..
-    ring.devices((e, devices) => { // making it multi devices
-        ring.history((e, history) => {
-            const fetch = (info, callback) => {
-                ring.recording(info.id, (e, recording) => {
-                    const file = path.join(base, '.', url.parse(recording).pathname);
-                    const dirname = path.dirname(file);
-                    mkdirp(dirname, function(err) {
-                        console.log('Fetching file', file);
-                        const writer = fs.createWriteStream(file);
-                        writer.on('close', () => {
-                            console.log('Done writing', file);
-                            callback();
-                        });
-                        request(recording).pipe(writer);
+    ring.history(historyLimit, (e, history) => {
+        const fetch = (info, callback) => {
+            ring.recording(info.id, (e, recording) => {
+                const file = path.join(base, '.', url.parse(recording).pathname);
+                const dirname = path.dirname(file);
+                mkdirp(dirname, () => {
+                    console.log('Fetching file', file);
+                    const writer = fs.createWriteStream(file);
+                    writer.on('close', () => {
+                        console.log('Done writing', file);
+                        callback();
                     });
+                    request(recording).pipe(writer);
                 });
-            };
-
-            async.eachLimit(history, 10, fetch, () => {
-                console.log('done');
             });
+        };
+
+        async.eachLimit(history, 10, fetch, () => {
+            console.log('done');
         });
     });
 });
